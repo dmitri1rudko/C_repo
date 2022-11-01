2da99543f541cfb07bb0523dfa7924c288627500
dmitri1rudko <434georgia@gmail.com>
2022-11-01 20:40:31 +0300

    malloc malloc bruh

---
+++ isMallocDidHisThing.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char *heapMemory = malloc(100);
+    if (NULL == heapMemory) {
+        perror("malloc failed bruh");
+    }
+    return 0;
+}
